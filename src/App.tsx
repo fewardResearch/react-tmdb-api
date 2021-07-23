06eb05e9da055fa3f2e315ef03c665ae602fedab
Kubilay <kubilaysabah@gmail.com>
2021-07-24 01:45:41 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
-import React, { FC, ReactElement } from 'react';
+// React
+import type { FC, ReactElement } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+// Pages
+import Home from './pages/Home';
 
 const App: FC = (): ReactElement => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header />
+
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+
+      <Footer />
+    </>
   );
 }
 
