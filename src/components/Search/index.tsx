16eb5625e40aa989f09289e9bd34f9f7b8ab85b2
Kubilay <kubilaysabah@gmail.com>
2021-07-24 18:35:04 +0300

    refactor: search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -4,7 +4,7 @@ import type { FC, ReactElement } from 'react';
 
 // Redux
 import { useAppDispatch } from '../../app/hooks';
-import { setData } from '../../features/data'
+import { setSearch } from '../../features/search'
 
 // Utils
 import { getData } from '../../utils/request'; 
@@ -22,12 +22,13 @@ const Search: FC = (): ReactElement => {
             <div className="flex-1">
                 <input type="text" className="focus:outline-none w-full text-lg py-2 px-5" value={keyword} onChange={(event) => setKeyword(event.currentTarget.value)} />
             </div>
-            <div >
+            <div>
                 <button className="focus:outline-none bg-red-600 text-white px-5 h-full" onClick={() => getData(endpoint({
                     type: Types.search,
                     name: Product.movies,
-                    keyword: keyword
-                })).then(data => dispatch(setData(data)))}>Search</button>
+                    keyword: keyword,
+                    page: 1
+                })).then(data => dispatch(setSearch(data)))}>Search</button>
             </div>
         </div>
     );
