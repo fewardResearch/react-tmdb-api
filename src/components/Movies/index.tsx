695cf4c84bcda83e806d04ff66bd32d88716584b
Kubilay <kubilaysabah@gmail.com>
2021-07-25 00:29:09 +0300

    refactor: movies

--- src/components/Movies/index.tsx
+++ src/components/Movies/index.tsx
@@ -15,6 +15,7 @@ import { getData } from '../../utils/request';
 
 // Components
 import MovieCard from '../MovieCard';
+import PopularMovieList from '../PopularMovieList';
 
 const Movies: FC = (): ReactElement => {
     const dispatch = useAppDispatch();
@@ -28,19 +29,16 @@ const Movies: FC = (): ReactElement => {
             name: Product.popular,
             page: 1
         })).then(data => dispatch(setPopularMovies(data)));
-    }, []); // Component mounted
+    }, [dispatch]);
 
     return(
         <>
-            <div className="md:flex md:flex-wrap md:-mx-5">
-                {
-                    popularMovieList.value.length > 0 && popularMovieList.value[0].results.map((movie) => (
-                        <div className="md:w-1/2 lg:w-1/4 md:px-5 md:mb-10" key={movie.id}>
-                            <MovieCard movie={movie} />
-                        </div>
-                    ))
-                }
-            </div>
+            <PopularMovieList 
+                movieList={popularMovieList.value[0]?.results} 
+                total_pages={popularMovieList.value[0]?.total_pages}
+                total_results={popularMovieList.value[0]?.total_results} 
+                slider
+                />
 
             <div className="md:flex md:flex-wrap md:-mx-5">
                 {
