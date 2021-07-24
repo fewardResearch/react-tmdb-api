2f74be549d88aeec99df48a74b4756ef61af6cb9
Kubilay <kubilaysabah@gmail.com>
2021-07-24 18:34:39 +0300

    refactor: header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ import Search from '../Search'
 const Header: FC = (): ReactElement => {
     return(
         <header>
-            <div className="container mx-auto">
+            <div className="container mx-auto px-5 md:px-0">
                 <div className="lg:flex lg:justify-center py-5">
                     <div className="lg:w-1/2">
                         <Search />
