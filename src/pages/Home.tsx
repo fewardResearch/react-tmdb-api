c5026a11a9ac0b543d212efc3f5823c1fb58732c
Kubilay <kubilaysabah@gmail.com>
2021-07-24 18:35:12 +0300

    style: home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
+// React
 import type { FC, ReactElement } from 'react';
+
+// Components
+import Movies from '../components/Movies';
 
 const Home: FC = (): ReactElement => {
     return(
-        <>
-        
-        </>
+        <div className="container mx-auto px-5 md:px-0">
+            <Movies />
+        </div>
     );
 };
 
