9584a0b6e317e9bade53f119baed43c6df3a6f4e
Kubilay <kubilaysabah@gmail.com>
2021-07-24 18:35:18 +0300

    refactor: request

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,24 +1,27 @@
 import { IEndpointOptions } from '../types/function';
 
-import { Product, Types } from '../types/enum'
+import { Product, Types, Languages } from '../types/enum'
 
 const endpoint = (options: IEndpointOptions): string => {
+    const apiKey = "350f2a9d0b23b6c84ff314ab8c6c7ac2";
     const baseURL = "https://api.themoviedb.org/3/";
 
     if(options.type === Types.search && options.name === Product.movies) {
-        return baseURL + "search/movie?api_key=350f2a9d0b23b6c84ff314ab8c6c7ac2&language=en-US&page=1&include_adult=false&query=" + encodeURI(options.keyword);
+        return baseURL + `search/movie?api_key=${apiKey}&language=${Languages.tr}&page=${options.page}&include_adult=false${options.keyword ? "&query=" + encodeURI(options.keyword) : ""}`;
+    }
+    
+    else if(options.type === Types.movies && options.name === Product.popular) {
+        return baseURL + `movie/popular?api_key=${apiKey}&language=${Languages.tr}&page=${options.page}&include_adult=false${options.keyword ? "&query=" + encodeURI(options.keyword) : ""}`;
     }
 
-    return baseURL + "search/movie?api_key=350f2a9d0b23b6c84ff314ab8c6c7ac2&language=en-US&page=1&include_adult=false&query=" + encodeURI(options.keyword);
+    return baseURL + `movie/popular?api_key=${apiKey}&language=${Languages.tr}&page=${options.page}&include_adult=false${options.keyword ? "&query=" + encodeURI(options.keyword) : ""}`;
 };
 
 const getData = (endpoint: string) => {
-    const resData = fetch(endpoint)
+    return fetch(endpoint)
     .then(response => response.json())
     .then(data => data)
     .catch(error => console.error(error));
-
-    return resData;
 };
 
 export {
