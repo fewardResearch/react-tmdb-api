d93fbc90b51c1161416ca76349d98bf4de923f14
Kubilay <kubilaysabah@gmail.com>
2021-07-25 00:28:31 +0300

    refactor: utils ui

--- src/utils/UI.ts
+++ src/utils/UI.ts
@@ -3,6 +3,15 @@ const imagePath = (src: string | null): string => {
     return "https://via.placeholder.com/500x700";
 };
 
+const breakpoints = {
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    xxl: 1536
+};
+
 export {
-    imagePath
+    imagePath,
+    breakpoints
 }
