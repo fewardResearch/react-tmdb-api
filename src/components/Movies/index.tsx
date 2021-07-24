a340f84392287e27642b521cc699ae0f86d0e1c4
Kubilay <kubilaysabah@gmail.com>
2021-07-24 18:34:58 +0300

    feature: movies component

---
+++ src/components/Movies/index.tsx
@@ -0,0 +1,58 @@
+// React
+import { useEffect } from 'react';
+import type { FC, ReactElement } from 'react';
+
+// Redux
+import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { setPopularMovies } from '../../features/popularMovies';
+
+// Types
+import { Types, Product } from '../../types/enum'
+
+// Utils
+import { endpoint } from '../../utils/request';
+import { getData } from '../../utils/request';
+
+// Components
+import MovieCard from '../MovieCard';
+
+const Movies: FC = (): ReactElement => {
+    const dispatch = useAppDispatch();
+
+    const searchMovieList = useAppSelector((state) => state.search.value);
+    const popularMovieList = useAppSelector((state) => state.popularMovies);
+
+    useEffect(() => {
+        getData(endpoint({
+            type: Types.movies,
+            name: Product.popular,
+            page: 1
+        })).then(data => dispatch(setPopularMovies(data)));
+    }, []); // Component mounted
+
+    return(
+        <>
+            <div className="md:flex md:flex-wrap md:-mx-5">
+                {
+                    popularMovieList.value.length > 0 && popularMovieList.value[0].results.map((movie) => (
+                        <div className="md:w-1/2 lg:w-1/4 md:px-5 md:mb-10" key={movie.id}>
+                            <MovieCard movie={movie} />
+                        </div>
+                    ))
+                }
+            </div>
+
+            <div className="md:flex md:flex-wrap md:-mx-5">
+                {
+                    searchMovieList.length > 0 && searchMovieList[0].results.map((movie) => (
+                        <div className="md:w-1/2 lg:w-1/4 md:px-5 md:mb-10" key={movie.id}>
+                            <MovieCard movie={movie} />
+                        </div>
+                    ))
+                }
+            </div>
+        </>
+    );
+};
+
+export default Movies;
